f7940a55bf11f28816a1ea551f9468eb83783b6e
Piotr Czerski <blater@poczta.fm>
2017-07-01 14:44:56 +0200

    koniec

---
+++ js/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// Nie jest konieczne, chyba że chcemy zmienić jakiś parametr, można to też zrobić z poziomu html
+    $('.carousel').carousel({
+        interval: 2000,
+        pause: 'hover'
+    });
